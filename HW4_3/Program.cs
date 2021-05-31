bd1a2b5eb0626bcd25b8726c118fbbc6fd84caed
Sergej Venhura <71256928+venguraserg@users.noreply.github.com>
2021-05-31 17:22:16 +0300

    работа с матрицами

--- HW4_3/Program.cs
+++ HW4_3/Program.cs
@@ -10,6 +10,64 @@ namespace HW4_3
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Введите размерность матрицы");
+            Console.WriteLine("Введите количество строк");
+            var rowNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите количество столбцов");
+            var columnNumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Дабы вас не перетруждать заполним случайным образом");
+            Random randomNumber = new Random();
+
+            int[,] matrix_1 = new int[rowNumber, columnNumber];
+            int[,] matrix_2 = new int[rowNumber, columnNumber];
+            int[,] matrix_3 = new int[rowNumber, columnNumber];
+            Console.WriteLine("Матрица 1:");
+            for (var i=0; i<rowNumber; i++)
+            {
+                for(var j=0; j<columnNumber; j++)
+                {
+                    matrix_1[i, j] = randomNumber.Next(100);
+                    Console.Write(matrix_1[i, j] >= 100 ? ("  ") : matrix_1[i, j] >= 10 ? ("   ") : ("    "));
+                    Console.Write($"{matrix_1[i,j]}"); 
+                    
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("Матрица 2:");
+            for (var i = 0; i < rowNumber; i++)
+            {
+                for (var j = 0; j < columnNumber; j++)
+                {
+                    matrix_2[i, j] = randomNumber.Next(100);
+                    Console.Write(matrix_2[i, j] >= 100 ? ("  ") : matrix_2[i, j] >= 10 ? ("   ") : ("    "));
+                    Console.Write($"{matrix_2[i, j]}");
+                    
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("\n\nДля продолженния нажмите любую клавишу...");
+            Console.ReadKey();
+
+            Console.Clear();
+            Console.WriteLine("Умножение матрицы на число:");
+            Console.WriteLine("Введите число на которое умножим");
+            var userNumber = Convert.ToInt32(Console.ReadLine());
+            for (var i = 0; i < rowNumber; i++)
+            {
+                for (var j = 0; j < columnNumber; j++)
+                {
+                    matrix_3[i, j] = matrix_1[i, j] * userNumber;
+                    Console.Write(matrix_3[i, j] >= 100 ? ("  ") : matrix_3[i, j] >= 10 ? ("   ") : ("    "));
+                    Console.Write($"{matrix_3[i, j]}");
+                   
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("\n\nДля продолженния нажмите любую клавишу...");
+            Console.ReadKey();
+
+
         }
     }
 }
