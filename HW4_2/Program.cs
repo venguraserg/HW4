aac21ed988982f10b87e245d48468a2ba7723be1
Sergej Venhura <71256928+venguraserg@users.noreply.github.com>
2021-05-31 15:36:13 +0300

    треугольник паскаля

--- HW4_2/Program.cs
+++ HW4_2/Program.cs
@@ -10,6 +10,31 @@ namespace HW4_2
     {
         static void Main(string[] args)
         {
+
+            int c = 1;
+
+            Console.Write("Введите количество строк: ");
+            var nunberRow = int.Parse(Console.ReadLine());
+            for (var i = 0; i < nunberRow; i++)
+            {
+                for (var emptyCell = 1; emptyCell <= nunberRow - i; emptyCell++)
+                    Console.Write("  ");
+
+
+                for (var j = 0; j <= i; j++)
+                {
+                    if (j == 0 || i == 0)
+                        c = 1;
+                    else
+                        c = c * (i - j + 1) / j;
+                    Console.Write(c);
+                    Console.Write(c >= 100 ? (" ") : c >= 10 ? ("  ") : ("   "));
+
+                }
+                Console.Write("\n");
+            }
+
+            Console.ReadKey();
         }
     }
 }
