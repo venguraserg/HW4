9b855c51132a76c2b12ddba7b63c9e467acad9a8
Sergej Venhura <71256928+venguraserg@users.noreply.github.com>
2021-06-10 16:41:48 +0300

    Треугольник паскаля испрвлен

--- HW4_2/Program.cs
+++ HW4_2/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +9,23 @@ namespace HW4_2
 {
     class Program
     {
+        // Чать кода взята на стекОверфлоу для максимизации консоли
+        // Что тут происходит до конца не знаю)))
+        // но работает
+        [DllImport("kernel32.dll", ExactSpelling = true)]
+        private static extern IntPtr GetConsoleWindow();
+        private static IntPtr ThisConsole = GetConsoleWindow();
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+        private const int MAXIMIZE = 3;
+        
+
+
         static void Main(string[] args)
         {
+            //Увеличивает экран для красивого вывода
+            Console.SetWindowSize(Console.LargestWindowWidth, Console.LargestWindowHeight);
+            ShowWindow(ThisConsole, MAXIMIZE);
 
             int c = 1;
 
@@ -18,7 +34,7 @@ namespace HW4_2
             for (var i = 0; i < nunberRow; i++)
             {
                 for (var emptyCell = 1; emptyCell <= nunberRow - i; emptyCell++)
-                    Console.Write("  ");
+                    Console.Write($"{"",4}");  // для красивого вывода примеим интерполяцию строк
 
 
                 for (var j = 0; j <= i; j++)
@@ -27,8 +43,8 @@ namespace HW4_2
                         c = 1;
                     else
                         c = c * (i - j + 1) / j;
-                    Console.Write(c);
-                    Console.Write(c >= 100 ? (" ") : c >= 10 ? ("  ") : ("   "));
+                    Console.Write($"{c,8}");// для красивого вывода примеим интерполяцию строк
+                    //Console.Write(c >= 100 ? (" ") : c >= 10 ? ("  ") : ("   ")); 
 
                 }
                 Console.Write("\n");
