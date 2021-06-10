72256efccde97b061ddaaa9cfcdfbb459851bbbe
Sergej Venhura <71256928+venguraserg@users.noreply.github.com>
2021-06-10 17:21:31 +0300

    необходимо доделать умножение матриц

--- HW4_3/Program.cs
+++ HW4_3/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace HW4_3
 {
@@ -10,15 +7,31 @@ namespace HW4_3
     {
         static void Main(string[] args)
         {
+            bool correctParse;
             #region Умножение матрицы на число
             //Умножение матрицы на число
             Console.Clear();
             Console.WriteLine("Умножение матрицы на число");
             Console.WriteLine("Введите размерность для матрицы");
             Console.WriteLine("Введите количество строк");
-            var rowNumberMatrix = Convert.ToInt32(Console.ReadLine());     // ввод количества строк
+            int rowNumberMatrix, columnNumberMatrix;
+            
+            do 
+            {
+                correctParse = int.TryParse(Console.ReadLine(), out rowNumberMatrix); // ввод количества строк
+                if (rowNumberMatrix <= 0) Console.WriteLine("Не коррекные данные, повторите ввод");
+            }
+            while (!correctParse || rowNumberMatrix<=0);
+                        
             Console.WriteLine("Введите количество столбцов");
-            var columnNumberMatrix = Convert.ToInt32(Console.ReadLine());  // ввод количества столбцов
+
+            do
+            {
+                correctParse = int.TryParse(Console.ReadLine(), out columnNumberMatrix); // ввод количества столбцов
+                if (columnNumberMatrix <= 0) Console.WriteLine("Не коррекные данные, повторите ввод");
+            }
+            while (!correctParse ||columnNumberMatrix <= 0);
+            
             //Формируем и выводим на экран матрицу случайным образом
             Console.WriteLine("Дабы вас не перетруждать заполним случайным образом");
             Random randomNumber = new Random();
@@ -29,8 +42,8 @@ namespace HW4_3
                 for (var j = 0; j < columnNumberMatrix; j++)
                 {
                     matrix[i, j] = randomNumber.Next(100);
-                    Console.Write(matrix[i, j] >= 100 ? ("  ") : matrix[i, j] >= 10 ? ("   ") : ("    "));
-                    Console.Write($"{matrix[i, j]}");
+                    //Console.Write(matrix[i, j] >= 100 ? ("  ") : matrix[i, j] >= 10 ? ("   ") : ("    "));
+                    Console.Write($"{matrix[i, j],5}");
 
                 }
                 Console.WriteLine();
@@ -46,8 +59,8 @@ namespace HW4_3
                 for (var j = 0; j < columnNumberMatrix; j++)
                 {
                     newMatrix[i, j] = matrix[i, j] * userNumber;
-                    Console.Write(newMatrix[i, j] >= 100 ? ("  ") : newMatrix[i, j] >= 10 ? ("   ") : ("    "));
-                    Console.Write($"{newMatrix[i, j]}");
+                    //Console.Write(newMatrix[i, j] >= 100 ? ("  ") : newMatrix[i, j] >= 10 ? ("   ") : ("    "));
+                    Console.Write($"{newMatrix[i, j],7}");
 
                 }
                 Console.WriteLine();
@@ -58,14 +71,30 @@ namespace HW4_3
 
             //Сложение и вычитание матриц
             #region Сложение и вычитание матриц
+            int rowNumberMatrix1, columnNumberMatrix1;
+
             Console.Clear();
             Console.WriteLine("Сложение матриц");
             Console.WriteLine("Введите размерность для матриц");
             Console.WriteLine("Складывать и вычетать можно только матрицы одинаковой размерности \nВведите размерность для матрицы 1 и 2");
             Console.WriteLine("Введите количество строк");
-            var rowNumberMatrix1 = Convert.ToInt32(Console.ReadLine());     // ввод количества строк
+
+            do
+            {
+                correctParse = int.TryParse(Console.ReadLine(), out rowNumberMatrix1); // ввод количества строк
+                if (rowNumberMatrix1 <= 0) Console.WriteLine("Не коррекные данные, повторите ввод");
+            }
+            while (!correctParse || rowNumberMatrix1 <= 0);
+            
             Console.WriteLine("Введите количество столбцов");
-            var columnNumberMatrix1 = Convert.ToInt32(Console.ReadLine());  // ввод количества столбцов
+
+            do
+            {
+                correctParse = int.TryParse(Console.ReadLine(), out columnNumberMatrix1); // ввод количества столбцов
+                if (columnNumberMatrix1 <= 0) Console.WriteLine("Не коррекные данные, повторите ввод");
+            }
+            while (!correctParse || columnNumberMatrix1 <= 0);
+            
             //Формируем и выводим на экран матрицу случайным образом
             Console.WriteLine("Дабы вас не перетруждать заполним случайным образом");
             int[,] matrix_1 = new int[rowNumberMatrix1, columnNumberMatrix1];
@@ -85,8 +114,8 @@ namespace HW4_3
                 for (var j = 0; j < columnNumberMatrix1; j++)
                 {
                     matrix_1[i, j] = randomNumber.Next(100);
-                    Console.Write(matrix_1[i, j] >= 100 ? ("  ") : matrix_1[i, j] >= 10 ? ("   ") : ("    "));
-                    Console.Write($"{matrix_1[i, j]}");
+                    //Console.Write(matrix_1[i, j] >= 100 ? ("  ") : matrix_1[i, j] >= 10 ? ("   ") : ("    "));
+                    Console.Write($"{matrix_1[i, j],5}");
 
                 }
                 Console.WriteLine();
@@ -97,8 +126,8 @@ namespace HW4_3
                 for (var j = 0; j < columnNumberMatrix1; j++)
                 {
                     matrix_2[i, j] = randomNumber.Next(100);
-                    Console.Write(matrix_2[i, j] >= 100 ? ("  ") : matrix_2[i, j] >= 10 ? ("   ") : ("    "));
-                    Console.Write($"{matrix_2[i, j]}");
+                    //Console.Write(matrix_2[i, j] >= 100 ? ("  ") : matrix_2[i, j] >= 10 ? ("   ") : ("    "));
+                    Console.Write($"{matrix_2[i, j],5}");
                 }
                 Console.WriteLine();
             }
@@ -109,8 +138,8 @@ namespace HW4_3
                 for (var j = 0; j < columnNumberMatrix1; j++)
                 {
                     matrix_1plus2[i, j] = matrix_1[i, j] + matrix_2[i, j];
-                    Console.Write(matrix_1plus2[i, j] >= 100 ? ("  ") : matrix_1plus2[i, j] >= 10 ? ("   ") : ("    "));
-                    Console.Write($"{matrix_1plus2[i, j]}");
+                    //Console.Write(matrix_1plus2[i, j] >= 100 ? ("  ") : matrix_1plus2[i, j] >= 10 ? ("   ") : ("    "));
+                    Console.Write($"{matrix_1plus2[i, j],5}");
                 }
                 Console.WriteLine();
             }
@@ -121,8 +150,8 @@ namespace HW4_3
                 for (var j = 0; j < columnNumberMatrix1; j++)
                 {
                     matrix_1plus2[i, j] = matrix_1[i, j] - matrix_2[i, j];
-                    Console.Write((matrix_1plus2[i, j] >= 100 || matrix_1plus2[i, j] < -9) ? ("  ") : (matrix_1plus2[i, j] >= 10 || matrix_1plus2[i, j] < 0) ? ("   ") : ("    "));
-                    Console.Write($"{matrix_1plus2[i, j]}");
+                    //Console.Write((matrix_1plus2[i, j] >= 100 || matrix_1plus2[i, j] < -9) ? ("  ") : (matrix_1plus2[i, j] >= 10 || matrix_1plus2[i, j] < 0) ? ("   ") : ("    "));
+                    Console.Write($"{matrix_1plus2[i, j],5}");
                 }
                 Console.WriteLine();
             }
