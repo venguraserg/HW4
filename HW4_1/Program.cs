d60e976d1519b6b364b545018d46e428f3962ada
Венгура Сергей <venguraserg@mail.ru>
2021-06-13 23:21:52 +0300

    Убран старый лишний код

--- HW4_1/Program.cs
+++ HW4_1/Program.cs
@@ -65,45 +65,13 @@ namespace HW4_1
             {
                 
                 Console.Write($"| {(Months)massive[i, 0],-10}");                                                            //Выводим месяц через ENUM
-                //for (var k = 0; k < (10 - (((Months)massive[i, 0]).ToString()).Length); k++) { Console.Write(" "); }    //Считаем отступы, чтобы таблица была ровной
                 
                 Console.Write($"|     {massive[i, 1],-10}");                                //выводим доход и распаолаем ровно в таблице
-                //var number = massive[i, 1];                                          // получаем колличество символов числа
-                //int count = 0;
-                //if (number < 0) number = -number;
-                //while (number > 0)
-                //{
-                //    number = number / 10;
-                //    count++;
-                //}
-                //for (var k = 0; k < (10 - count); k++) { Console.Write(" "); }          //Расчитываем отступ
-
+                
                 Console.Write($"|     {massive[i, 2],-10}");                                //Далее анологично для остальных столбцов
-                //number = massive[i, 2];
-                //count = 0;
-                //if (number < 0) number = -number;
-                //while (number > 0)
-                //{
-                //    number = number / 10;
-                //    count++;
-                //}
-                //for (var k = 0; k < (11 - count); k++) { Console.Write(" "); }
-
+                
                 Console.Write($"|     {massive[i, 3],-10}"+"|\n");
-                //number = massive[i, 3];
-                //count = 0;
-                //if (number < 0) 
-                //{
-                //    number = -number;
-                //    count++;
-                //}
-                //while (number > 0)
-                //{
-                //    number = number / 10;
-                //    count++;
-                //}
-                //for (var k = 0; k < (13 - count); k++) { Console.Write(" "); }
-                //Console.WriteLine("*");
+                
             }
             Console.WriteLine("-------------------------------------------------------------");
 
@@ -167,48 +135,6 @@ namespace HW4_1
                 if(reportMassive[i,0]!=0)Console.WriteLine($" {(Months)reportMassive[i, 0],-10}{reportMassive[i, 1],5}");
             }
            
-           
-
-
-
-
-
-
-
-            //for (int i = 0, count = 0; i < 12; i++)
-            //{
-            //    if ((tempArray[i] != tempArray[i + 1])/*&&(tempArray[i]>=0)*/)   //Если раскоментировать, то будем искать только прибыль больше нуля
-            //    {                                                                //Сдесь есть баг, так как числа генерятся рандомно, 
-            //        reportMassive[count, 0] = tempArray[i];                      // бывает такое что месяцев с положительной прибылью менее трех
-            //        count++;                                                     // так как массив предопределен заранее, в этом случае выкидывает исключение
-            //    }                                                                // в принципе как решить знаю, если нужно - реализую
-
-            //    if (count > 2) break;
-            //}
-
-            //for (var i = 0; i < 12; i++)
-            //{
-            //    for (var j = 0; j < 12; j++)
-            //    {
-            //        if (reportMassive[i, 0] == massive[j, 3]) 
-            //        {
-            //            //reportMassive[i, 0] = massive[j, 0];
-            //            reportMassive[i, 1] = massive[j, 0]; 
-            //        }
-
-            //    }
-
-            //}
-
-            //Console.WriteLine("Наименьшая прибыль (Наибольший убыток) по возрастанию по месяцам");
-
-            //for (var i = 0; i < 12; i++)
-            //{
-            //    Console.WriteLine($" {(Months)reportMassive[i, 1],-10}{reportMassive[i, 0],5}");
-            //    //for (var k = 0; k < (10 - (((Months)reportMassive[i, 1]).ToString()).Length); k++) { Console.Write(" "); }
-            //    //Console.WriteLine($" {reportMassive[i, 0]}");
-            //}
-
 
             Console.WriteLine("\n\nДля продолжения нажмите любую клавишу . . . ");
             Console.ReadKey();
